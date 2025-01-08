ae7586d51737c6fedcb9a68b4188861c451ce064
NazmulIslam95 <nazmulislamctg@gmail.com>
2025-01-08 19:45:00 +0600

    navbar edited

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -44,139 +44,84 @@ const Navbar = () => {
     </>
   );
 
+  const navIcons = (
+    <>
+      {/* ----------- search button ---------- */}
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="">
+          <div className="indicator text-2xl">
+            <TfiSearch />
+          </div>
+        </div>
+      </div>
+      {/* ----------- profile button ---------- */}
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="">
+          <div className="indicator text-3xl ">
+            <CiUser />
+          </div>
+        </div>
+      </div>
+
+      {/* ----------- cart button ---------- */}
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="">
+          <div className="indicator text-2xl">
+            <BsBucket />
+          </div>
+        </div>
+      </div>
+
+      {/* ----------- favorite button ---------- */}
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="">
+          <div className="indicator text-3xl">
+            <CiHeart />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+
   return (
     <div className="max-w-7xl mx-auto">
       <div className="navbar p-0 justify-between bg-base-100 ">
         <div className="flex">
+          <div className="dropdown ">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {navItems}
+              {navIcons}
+            </ul>
+          </div>
           <Link to="/">
-            <img src="/logo.png" alt="logo image" className="w-48" />
+            <img src="/logo.png" alt="logo image" className="w-28 lg:w-48" />
           </Link>
         </div>
-        {/* <Navitems></Navitems> */}
-        <div className="flex-none space-x-6 ">
-          {/* ----------- search button ---------- */}
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="">
-              <div className="indicator text-2xl">
-                <TfiSearch />
-              </div>
-            </div>
-            {/* <div
-            tabIndex={0}
-            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div> */}
-          </div>
 
-          {/* ----------- profile button ---------- */}
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="">
-              <div className="indicator text-3xl ">
-                <CiUser />
-              </div>
-            </div>
-            {/* <div
-            tabIndex={0}
-            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div> */}
-          </div>
-
-          {/* ----------- cart button ---------- */}
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="">
-              <div className="indicator text-2xl">
-                <BsBucket />
-              </div>
-            </div>
-            {/* <div
-            tabIndex={0}
-            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div> */}
-          </div>
-
-          {/* ----------- favorite button ---------- */}
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="">
-              <div className="indicator text-3xl">
-                <CiHeart />
-              </div>
-            </div>
-            {/* <div
-            tabIndex={0}
-            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div> */}
-          </div>
-
-          {/* <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className=" avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div> */}
-        </div>
+        <div className="hidden lg:flex space-x-4 ">{navIcons}</div>
       </div>
       {/* <Navitems>-----------------------------------------</Navitems> */}
       <div className="hidden lg:flex justify-center border border-black p-2 border-x-0">
-        {/* <div> */}
-        <ul className="flex items-center space-x-10">
-          {navItems}
-        </ul>
+        <ul className="flex items-center space-x-10">{navItems}</ul>
       </div>
 
       {/* ------------------------------------------- */}
@@ -194,45 +139,44 @@ const Navbar = () => {
           <h4 className="font-semibold">Support 24/7</h4>
         </div>
       </div>
-    </div>
 
-    // <div className="navbar bg-base-100">
-    //   <div className="navbar-start">
-    //     <div className="dropdown">
-    //       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-    //         <svg
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           className="h-5 w-5"
-    //           fill="none"
-    //           viewBox="0 0 24 24"
-    //           stroke="currentColor"
-    //         >
-    //           <path
-    //             strokeLinecap="round"
-    //             strokeLinejoin="round"
-    //             strokeWidth="2"
-    //             d="M4 6h16M4 12h8m-8 6h16"
-    //           />
-    //         </svg>
-    //       </div>
-    //       <ul
-    //         tabIndex={0}
-    //         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-    //       >
-    //         {navItems}
-    //       </ul>
-    //     </div>
-    //     <a className="btn btn-ghost text-xl">daisyUI</a>
-    //   </div>
-    //   <div className="navbar-center hidden lg:flex">
-    //     <ul className="menu menu-horizontal px-1 flex items-center space-x-10">
-    //       {navItems}
-    //     </ul>
-    //   </div>
-    //   <div className="navbar-end">
-    //     <a className="btn">Button</a>
-    //   </div>
-    // </div>
+      {/* ------------------------------- */}
+      {/* <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {navItems}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="flex items-center space-x-10">{navItems}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="hidden lg:fflex items-center">{navIcons}</a>
+        </div>
+      </div> */}
+    </div>
   );
 };
 
